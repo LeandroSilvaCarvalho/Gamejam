f1124047b6a916f5302d2ad31bb32ce5bcee0530
LeandroSilvaCarvalho <leandrosdc.pro@gmail.com>
2022-12-04 20:24:01 +0100

    add - start menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class GameManager : SingletonBehaviour<GameManager>, IGameManager
+{
+    [SerializeField] private string startScene;
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(startScene);
+    }
+
+}
